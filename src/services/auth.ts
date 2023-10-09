d1ddd8e31a5934d92ba2397704e892caab0dfcc4
Yunsun Jung <yunsun992510@gmail.com>
2023-10-09 19:55:41 +0900

    [Feat] Hashing 구현

---
+++ src/services/auth.ts
@@ -0,0 +1,22 @@
+import { pbkdf2, randomBytes } from "node:crypto";
+
+type HashResult = {
+  derivedKey: string; // length: 128
+  salt: string; // length: 64
+};
+
+const hashPass = (password: string, _salt?: string) => {
+  return new Promise<HashResult>((resolve, reject) => {
+    const secret = Buffer.from(password, "utf-8");
+    const salt = _salt ? Buffer.from(_salt, "hex") : randomBytes(32);
+
+    pbkdf2(secret, salt, 600000, 64, "sha256", (err, derivedKey) => {
+      if (err) return reject(err);
+
+      resolve({
+        derivedKey: derivedKey.toString("hex"),
+        salt: salt.toString("hex");
+      });
+    });
+  });
+};
