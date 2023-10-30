1ff0276a49b0709cd117234438e0557f8e53d627
Yunsun Jung <yunsun992510@gmail.com>
2023-10-31 00:54:42 +0900

    [Feat] loginEmail API validation 구현

--- src/middlewares/validator.ts
+++ src/middlewares/validator.ts
@@ -4,6 +4,29 @@ import ServerError from "../services/error";
 import { User } from "../models/User.model";
 import { errorHandler } from "../controllers/error";
 
+export const validateLoginEmail = (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    if (!req.body) throw new ServerError("AUTH__MISSING_BODY", 400);
+    const email = req.body.email;
+    if (typeof email !== "string")
+      throw new ServerError("AUTH__INVALID_EMAIL_TYPE", 400);
+    if (!isEmail(email)) throw new ServerError("AUTH__INVALID_EMAIL", 400);
+    const password = req.body.password;
+    if (typeof password !== "string")
+      throw new ServerError("AUTH__INVALID_PW_TYPE", 400);
+    if (password.length === 0 || password.length > 128)
+      throw new ServerError("AUTH__INVALID_PW", 400);
+
+    next(); // validation success
+  } catch (error) {
+    next(error);
+  }
+};
+
 export const validateCreateAccount = async (
   req: Request,
   res: Response,
