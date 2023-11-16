977bae8163f036a33b326877a65a79f2b43ab90c
Yunsun Jung <yunsun992510@gmail.com>
2023-11-16 12:14:51 +0900

    [Feat] createPost에서 친구 생성 기능까지 추가, 파일 및 친구이름 받는 부분 구현

--- src/controllers/posts.ts
+++ src/controllers/posts.ts
@@ -4,6 +4,7 @@ import ServerError from "../services/error";
 import { Post } from "../models/Post.model";
 import { User } from "../models/User.model";
 import { sequelize } from "../services/database";
+import { Friend, FriendCAttribs } from "../models/Friend.model";
 
 const postsController = {
   viewPosts: async (req: Request, res: Response, next: NextFunction) => {
@@ -11,20 +12,14 @@ const postsController = {
       const user = req.user;
       if (!user) throw new ServerError("UNAUTHENTICATED", 401);
 
-      // await user.$get("posts", { include: [User] }); // user.posts에 값 넣어줌
-
       const posts = await Post.findAll({
         where: { createdById: user.id },
         include: [{ model: User, foreignKey: "createdById" }],
-        // include: [User],
-        // include: User 와 같은 뜻!
-        // post만 불러오지 않고 user 정보 전체 불러오는 역할!
-        // 만약 게시물이 없으면 findAll은 '빈 배열'을 반환함
       });
 
       res.status(200).json({
         success: true,
-        posts: posts.map((post) => post.toResponse()), // return 생략된 거 꼭 조심!
+        posts: posts.map((post) => post.toResponseSimple()), // return 생략된 거 꼭 조심!
       });
     } catch (error) {
       next(error);
@@ -40,13 +35,21 @@ const postsController = {
       const latitude = req.body.latitude;
       const longitude = req.body.longitude;
 
-      if (!req.file) {
-        throw new ServerError("POSTS__MISSING_PHOTO", 400);
-      }
-      const file = req.file as Express.MulterS3.File;
-      const fileName = file.key;
+      const newFriendNames = req.body.newFriendNames.split(","); // ["윤선", "건희"]
+      const oldFriendNames = req.body.oldFriendNames; // [""]
+      const friendNames = newFriendNames.concat(oldFriendNames); // ["건희", ""]
 
-      const postResponse = await sequelize.transaction(async (t) => {
+      const files = req.files as {
+        // req.files as Record<string, Express.MulterS3.File[]> 와 같은 표현
+        [fieldname: string]: Express.MulterS3.File[];
+      };
+      const originalImgPath = files.original[0].key.replace(/^images\//, "");
+      const faceImgPaths = files.faces.map((face) =>
+        face.key.replace(/^images\//, ""),
+      );
+      console.log(faceImgPaths);
+
+      const postResponse = await sequelize.transaction(async (transaction) => {
         const post = await Post.create(
           {
             content: req.body.content,
@@ -54,16 +57,49 @@ const postsController = {
             location: location,
             latitude: latitude,
             longitude: longitude,
-            imgPath: fileName,
+            imgPath: originalImgPath,
             createdById: user.id, // 외래키 직접 지정
-            friends: [],
           },
-          { transaction: t },
+          { transaction },
         );
+
+        const friends: Friend[] = [];
+        for (let i = 0; i < friendNames.length; i++) {
+          const friendName = friendNames[i];
+
+          if (!friendName) continue;
+
+          // findOrCreate가 리턴할 때 첫번째 인자로 생성하거나 찾은 친구를, 두번째 인자로 지금 새로 만든건지 여부를 돌려줌
+          const result = await Friend.findOrCreate({
+            // 찾을 때 사용할 데이터
+            where: {
+              friendName: friendName,
+              createdById: user.id,
+            },
+            // 만들 때 사용할 데이터
+            defaults: {
+              friendName: friendName,
+              imgPath: faceImgPaths[i],
+              createdById: user.id,
+            },
+            transaction,
+          });
+
+          const friend = result[0];
+          friends.push(friend);
+        }
+
+        // throughTable에 row가 하나 추가됨
+        // friends와 posts에 각각 서로 추가됨
+        for (const friend of friends) {
+          await friend.$add("posts", post, { transaction });
+        }
+
+        // const friends = await Friend.bulkCreate(friendsData, { transaction });
 
         // SELECT * FROM users WHERE users.id = 1(post.createdById);
 
-        await post.reload({ include: [User], transaction: t });
+        await post.reload({ include: [User, Friend], transaction });
 
         return post.toResponse();
       });
